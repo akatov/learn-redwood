c17dfefadf17b6862c79e86dd85ac8e80e963101
Dmitri Akatov <dmitri@akatov.com>
2022-06-02 12:03:43 +0100

    headers

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -6,14 +6,28 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <h1>AboutPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
-      </p>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h1>AboutPage</h1>
+        <p>
+          Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>about</code>, link to me with `
+          <Link to={routes.about()}>About</Link>`
+        </p>
+        <Link to={routes.home()}>Return home</Link>
+      </main>
     </>
   )
 }
