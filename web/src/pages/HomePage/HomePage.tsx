5a34138d3dab5d3a2b76cebb364f80dd9f56dd93
Dmitri Akatov <dmitri@akatov.com>
2022-06-02 23:02:25 +0100

    show articles cell on HomePage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import ArticlesCell from 'src/components/ArticlesCell'
+
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
+      <ArticlesCell />
 
       <h1>HomePage</h1>
       <p>
