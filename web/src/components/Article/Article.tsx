18a83e65dd22b5bbaa215b62b7e53c5058d89810
Dmitri Akatov <dmitri@akatov.com>
2022-06-02 23:37:52 +0100

    yarn rw g component Article

---
+++ web/src/components/Article/Article.tsx
@@ -0,0 +1,10 @@
+const Article = () => {
+  return (
+    <div>
+      <h2>{'Article'}</h2>
+      <p>{'Find me in ./web/src/components/Article/Article.tsx'}</p>
+    </div>
+  )
+}
+
+export default Article
