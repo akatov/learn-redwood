d7120620c8d8db9dcf01e7c9dbf94c01d9756196
Dmitri Akatov <dmitri@akatov.com>
2022-06-02 12:10:17 +0100

    add layout

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -6,28 +6,14 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <h1>AboutPage</h1>
-        <p>
-          Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
-        </p>
-        <p>
-          My default route is named <code>about</code>, link to me with `
-          <Link to={routes.about()}>About</Link>`
-        </p>
-        <Link to={routes.home()}>Return home</Link>
-      </main>
+      <h1>AboutPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>about</code>, link to me with `
+        <Link to={routes.about()}>About</Link>`
+      </p>
     </>
   )
 }
