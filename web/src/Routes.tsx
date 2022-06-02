8597a5c2b44ef78b520a31a1b46d1cad2c57ce96
Dmitri Akatov <dmitri@akatov.com>
2022-06-02 11:59:16 +0100

    change route of hello page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,7 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/" page={HomePage} name="home" />
+      <Route path="/hello" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
