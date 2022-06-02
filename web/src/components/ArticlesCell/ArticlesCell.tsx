3d27a6cb566cae0be5fb37d46f5fee3a6ad6b293
Dmitri Akatov <dmitri@akatov.com>
2022-06-02 23:00:25 +0100

    fix articles cell

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,10 +1,9 @@
 import type { ArticlesQuery } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
-
 export const QUERY = gql`
   query ArticlesQuery {
-    articles {
+    posts {
       id
     }
   }
@@ -18,10 +17,10 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+export const Success = ({ posts }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <ul>
-      {articles.map((item) => {
+      {posts.map((item) => {
         return <li key={item.id}>{JSON.stringify(item)}</li>
       })}
     </ul>
