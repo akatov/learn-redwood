d4b0410b5121db0adc963b1467465f7432debf3b
Dmitri Akatov <dmitri@akatov.com>
2022-06-03 08:22:17 +0100

    form interaction

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,19 +1,22 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Form, Submit, SubmitHandler, TextField } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
+interface FormValues {
+  input: string
+}
+
 const ContactPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
+      <Form onSubmit={onSubmit}>
+        <TextField name="input" />
+        <Submit>save</Submit>
+      </Form>
     </>
   )
 }
