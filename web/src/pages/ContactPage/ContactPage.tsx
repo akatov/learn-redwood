5c8758be833d2154da968fac986f7f4ba8434eb9
Dmitri Akatov <dmitri@akatov.com>
2022-06-03 11:18:31 +0100

    error styling for inpu

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,7 @@
 import {
+  FieldError,
   Form,
+  Label,
   Submit,
   SubmitHandler,
   TextAreaField,
@@ -20,14 +22,36 @@ const ContactPage = () => {
       <MetaTags title="Contact" description="Contact page" />
 
       <Form onSubmit={onSubmit}>
-        <label htmlFor="name">Name</label>
-        <TextField name="name" />
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
 
-        <label htmlFor="email">Email</label>
-        <TextField name="email" />
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
 
-        <label htmlFor="message">Message</label>
-        <TextAreaField name="message" />
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
         <Submit>save</Submit>
       </Form>
     </>
